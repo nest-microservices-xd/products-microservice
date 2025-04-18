3936e858237648f1274b352b2251b8ade869626c
César <sexareds@gmail.com>
2025-04-18 01:49:09 -0400

    migration

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,12 +3,14 @@ import {
   OnModuleInit,
   Logger,
   NotFoundException,
+  HttpStatus,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from 'generated/prisma/client';
 
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -53,7 +55,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: `Product with id #${id} not found`,
+      });
     }
 
     return product;
@@ -91,5 +96,21 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     // return this.product.delete({
     //   where: { id },
     // });
+  }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+    const products = await this.product.findMany({
+      where: { id: { in: ids }, available: true },
+    });
+
+    if (ids.length !== products.length) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: `Some products not found`,
+      });
+    }
+
+    return products;
   }
 }
